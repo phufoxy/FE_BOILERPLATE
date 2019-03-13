146fd19642129a9c99c321f2790c6ebe99d1d397
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2019-03-13 10:15:58 +0700

    write slice slider

---
+++ src/scripts/script.js
@@ -0,0 +1,10 @@
+$(document).on('ready', function () {
+    console.log(123);
+    $("#b-page-slider").slick({
+        dots: false,
+        infinite: false,
+        // speed: 1000,
+        // autoplay: 1000,
+        arrows: false
+    });
+});
